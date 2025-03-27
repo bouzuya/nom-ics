e8623777dd3aae405b030f1168f2009075197439
bouzuya <m@bouzuya.net>
2025-03-27 20:33:55 +0900

    Add contentline

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,37 @@
 use nom::{IResult, Parser};
 
+/// contentline   = name *(";" param ) ":" value CRLF
+/// ; This ABNF is just a general definition for an initial parsing
+/// ; of the content line into its property name, parameter list,
+/// ; and value string
+/// ; When parsing a content line, folded lines MUST first
+/// ; be unfolded according to the unfolding procedure
+/// ; described above.  When generating a content line, lines
+/// ; longer than 75 octets SHOULD be folded according to
+/// ; the folding procedure described above.
+///
+/// <https://datatracker.ietf.org/doc/html/rfc5545>
+fn contentline(input: &str) -> IResult<&str, (String, Vec<(String, Vec<String>)>, String)> {
+    (
+        name,
+        nom::multi::many0((nom::character::complete::char(';'), param)),
+        nom::character::complete::char(':'),
+        value,
+        nom::character::complete::line_ending,
+    )
+        .map(|(name, params, _, value, _)| {
+            (
+                name,
+                params
+                    .into_iter()
+                    .map(|(_, param)| param)
+                    .collect::<Vec<(String, Vec<String>)>>(),
+                value,
+            )
+        })
+        .parse(input)
+}
+
 /// iana-token    = 1*(ALPHA / DIGIT / "-")
 /// ; iCalendar identifier registered with IANA
 ///
@@ -217,6 +249,27 @@ fn x_name(input: &str) -> IResult<&str, String> {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_contentline() {
+        assert_eq!(
+            contentline("NAME;PARAM=value:VALUE\r\n"),
+            Ok((
+                "",
+                (
+                    "NAME".to_string(),
+                    vec![("PARAM".to_string(), vec!["value".to_string()])],
+                    "VALUE".to_string()
+                )
+            ))
+        );
+        assert_eq!(
+            contentline("NAME:VALUE\r\n"),
+            Ok(("", ("NAME".to_string(), vec![], "VALUE".to_string())))
+        );
+        assert!(contentline("NAME;PARAM=value:VALUE").is_err()); // Missing CRLF
+        assert!(contentline("NAME;PARAM=value").is_err()); // Missing ':' and CRLF
+    }
 
     #[test]
     fn test_iana_token() {
